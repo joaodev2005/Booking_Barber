07dbc9240e313ab9dbb7f0daf59d2a3a904412c2
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-02-05 17:51:44 -0300

    feat: add welcome message

--- my_booking/app/(home)/page.tsx
+++ my_booking/app/(home)/page.tsx
@@ -1,9 +1,21 @@
+import { format } from 'date-fns'
 import Header from "../_components/Header";
+import { pt } from 'date-fns/locale';
+import { ptBR } from 'date-fns/locale/pt-BR';
 
 export default function Home() {
   return (
     <>
       <Header />
+
+      <div className='p-5'>
+        <h2>Olá, seja bem vindo!</h2>
+        <p className='capitalize'>
+          {format(new Date(), 'EEEE, d MMMM', {
+            locale: ptBR
+          })}
+        </p>
+      </div>
     </>
   );
 }
