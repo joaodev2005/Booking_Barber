bd11523d2374fdba2f615385879e8f086d5760c2
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-02-13 15:25:39 -0300

    feat: add service booking

--- my_booking/app/barbershops/[id]/_components/ServiceItem.tsx
+++ my_booking/app/barbershops/[id]/_components/ServiceItem.tsx
@@ -1,14 +1,34 @@
 import { Button } from '@/app/_components/ui/button'
+import { Calendar } from '@/app/_components/ui/calendar'
 import { Card, CardContent } from '@/app/_components/ui/card'
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/app/_components/ui/sheet'
+import { ptBR } from 'date-fns/locale'
 import Image from 'next/image'
+import React, { useState } from 'react'
+import { generateDayTimeList } from '../_helpers/hours'
+import { format } from 'date-fns'
 
 const ServiceItem = ({ item }: { item: any }) => {
+    const [date, setDate] = React.useState<Date | undefined>(new Date())
+    const [hour, setHour] = useState<string | undefined>()
+
+    const timeList = generateDayTimeList(date!);
+
+    const handleDateClick = (date: Date | undefined) => {
+        setDate(date)
+        setHour(undefined)
+    }
+
+    const handleHourClick = (hour: string) => {
+        setHour(hour)
+    }
+
     return (
         <Card>
             <CardContent className='p-3'>
                 <div className="flex gap-4 items-center">
                     <div className="relative min-h-[110px] min-w-[110px] max-h-[110px] max-w-[110px]">
-                        <Image 
+                        <Image
                             src={item.imageUrl}
                             alt={item.name}
                             fill
@@ -22,9 +42,113 @@ const ServiceItem = ({ item }: { item: any }) => {
 
                         <div className="flex items-center justify-between mt-2">
                             <p className='font-bold text-primary text-sm'>R$ {item.price.toFixed(2)}</p>
-                            <Button variant="secondary">
-                                Reservar
-                            </Button>
+
+                            <Sheet>
+                                <SheetTrigger asChild>
+                                    <Button variant="secondary">
+                                        Reservar
+                                    </Button>
+                                </SheetTrigger>
+
+                                <SheetContent className='p-0'>
+                                    <SheetHeader className='text-left px-5 py-6 border-b border-solid border-secondary'>
+                                        <SheetTitle>Fazer Reserva</SheetTitle>
+                                    </SheetHeader>
+
+                                    <div className='py-6'>
+                                        <Calendar
+                                            mode="single"
+                                            selected={date}
+                                            onSelect={handleDateClick}
+                                            locale={ptBR}
+                                            fromDate={new Date()}
+                                            styles={{
+                                                head_cell: {
+                                                    width: '100%',
+                                                },
+                                                cell: {
+                                                    width: '100%',
+                                                },
+                                                button: {
+                                                    width: '100%',
+                                                },
+                                                nav_button_previous: {
+                                                    width: '32px',
+                                                    height: '32px',
+                                                },
+                                                nav_button_next: {
+                                                    width: '32px',
+                                                    height: '32px',
+                                                },
+                                                caption: {
+                                                    textTransform: 'capitalize',
+                                                }
+                                            }}
+                                        />
+                                    </div>
+
+                                    {date && (
+                                        <div className='flex gap-3 overflow-x-auto py-6 px-5 border-t border-solid border-secondary [&::-webkit-scrollbar]:hidden'>
+                                            {timeList.map((time) => (
+                                                <Button
+                                                    onClick={() => handleHourClick(time)}
+                                                    variant={
+                                                        hour === time ? 'default' : 'outline'
+                                                    }
+                                                    className='rounded-full'
+                                                    key={time}>
+                                                    {time}
+                                                </Button>
+                                            ))}
+                                        </div>
+                                    )}
+
+                                    <div className="py-5 px-5 border-t border-solid border-secondary">
+                                        <Card>
+                                            <CardContent className='p-3 flex flex-col gap-1'>
+                                                <div className="flex justify-between">
+                                                    <h2 className='font-bold text-sm'>{item.name}</h2>
+                                                    <h3 className='font-bold text-sm'>R$ {item.price.toFixed(2)}</h3>
+                                                </div>
+
+                                                {date && (
+                                                    <div className="flex justify-between">
+                                                        <h3 className='text-gray-400 text-sm'>Data</h3>
+                                                        <h4 className='text-sm'>
+                                                            {format(date, "dd 'de' MMMM", {
+                                                                locale: ptBR
+                                                            })}
+                                                        </h4>
+                                                    </div>
+                                                )}
+
+                                                {hour && (
+                                                    <div className="flex justify-between">
+                                                        <h3 className='text-gray-400 text-sm'>Horário</h3>
+                                                        <h4 className='text-sm'>
+                                                            {hour}
+                                                        </h4>
+                                                    </div>
+                                                )}
+
+                                                <div className="flex justify-between">
+                                                    <h3 className='text-gray-400 text-sm'>Barbearia</h3>
+                                                    <h4 className='text-sm'>
+                                                        Barbearia Vintage
+                                                    </h4>
+                                                </div>
+
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+
+                                    <SheetFooter className='px-5'>
+                                        <Button disabled={!date || !hour}>
+                                            Confirmar Reserva
+                                        </Button>
+                                    </SheetFooter>
+                                </SheetContent>
+                            </Sheet>
                         </div>
                     </div>
                 </div>
@@ -35,4 +159,8 @@ const ServiceItem = ({ item }: { item: any }) => {
 
 export default ServiceItem
 
+
+function useMemo(arg0: () => string[], arg1: (Date | undefined)[]) {
+    throw new Error('Function not implemented.')
+}
 
